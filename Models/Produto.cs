afdedf35242c2d3982108fe6d54508f0f234ae2f
Fernandes-Bernardo <bernardofpdev@gmail.com>
2025-12-09 14:55:59 -0300

    feat: creating produto model

--- Models/Produto.cs
+++ Models/Produto.cs
@@ -2,8 +2,22 @@ namespace SnackBar_system.Models;
 
     public class Produto
 {
+    public Produto(string nome, string categoria, double preco, int quantidadeTotal, int quantidadeDisponivel, int quantidadeReserva)
+    {
+        Id = Guid.NewGuid();
+        Nome = nome;
+        Categoria = categoria;
+        Preco = preco;
+        QuantidadeTotal = quantidadeTotal;
+        QuantidadeDisponivel = quantidadeDisponivel;
+        QuantidadeReserva = QuantidadeReserva;
+    }
+
     public Guid Id { get; init; }
-    public string Name { get; set; }
+    public string Nome { get; set; }
     public string Categoria { get; set; }
-    
+    public double Preco { get; set; }
+    public int QuantidadeTotal { get; set; }
+    public int QuantidadeDisponivel { get; set; }
+    public int QuantidadeReserva { get; set; }
 }
