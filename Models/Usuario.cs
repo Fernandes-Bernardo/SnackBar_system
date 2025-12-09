2d2af0384263348268ed410d2f94da97cd6204da
Fernandes-Bernardo <bernardofpdev@gmail.com>
2025-12-09 15:02:09 -0300

    feat: creating usuario model

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -1,1 +1,19 @@
 namespace SnackBar_system.Models;
+
+public class Usuario
+{
+    public Usuario(string nome, string email, string senha, string tipo)
+    {
+        Id = Guid.NewGuid();
+        Nome = nome;
+        Email = email;
+        Senha = senha;
+        Tipo = tipo;
+    }
+
+    public Guid Id { get; init; }
+    public string Nome { get; set; }
+    public string Email { get; set; }
+    public string Senha { get; set; }
+    public string Tipo { get; set; }
+}
